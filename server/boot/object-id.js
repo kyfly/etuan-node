3eced9693b077ac4e03957c99897541167a955ce
Zhenfei You <youzhenfei@126.com>
2015-02-01 22:04:33 +0800

    完成对模块的初步关系处理，待完善和调试

---
+++ server/boot/object-id.js
@@ -0,0 +1,8 @@
+/*
+var ObjectID = require('mongodb').ObjectID;
+
+module.exports = function setupGenerateId(server) {
+    server.dataSources.db.connector.generateId = function () {
+        return new ObjectID();
+    };
+};*/
