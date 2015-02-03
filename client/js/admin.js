4395879e147af5f0bb65c08590ec07bd1a5ecc3f
LiC <1078054388@qq.com>
2015-02-03 16:27:47 +0800

    修复了昨天提到的一些问题。现在sidebar在下屏幕会隐藏并且在nav上出现一个按钮，一些div的尺寸得到调整。暂时使用原生下载的bootstrap.min.css来看到效果（无论是修改过的还是原生sass编译以后都是不显示fonts）。

--- client/js/admin.js
+++ client/js/admin.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
   $('#mainHeight').css('min-height', $(window).outerHeight(true) - $('#nav').outerHeight(true) + "px");
+
   $('#contentHeight').css('min-height', $('#mainHeight').outerHeight(true) - $('#footer').outerHeight(true) + 70 + "px");
+
+  $('#menu').css('min-height',$('#mainHeight').outerHeight(true)  + 20 + "px");
+
+  $('#contentBtn').click(function(){
+    $("#menu").slideToggle();
+  });
 });
