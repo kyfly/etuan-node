c74f93114dce1c29a66460af45e6474d371395f6
evilucifero <x@evilucifero.com>
2015-02-07 22:50:48 +0800

    创建模拟注册功能

---
+++ client/js/submit/app.js
@@ -0,0 +1,20 @@
+function SubmitCtrl ($scope,$resource,$timeout,$window) {
+  var Submit = $resource('/api/OrganizationUsers');
+  $scope.submitBtn = function(){
+    Submit.save({
+      "email":this.email,
+      "password":this.password
+    },
+      function(res){
+        alert('模拟注册成功!');
+        console.log(res);
+        $timeout(function(){$window.location='/login.html'},5000);
+    },
+    function(res){
+      alert('模拟注册失败!');
+      console.log(res);
+    });
+  };
+}
+var app = angular.module('app', ['ngResource']);
+app.controller('SubmitCtrl',['$scope','$resource','$timeout','$window',SubmitCtrl]);
