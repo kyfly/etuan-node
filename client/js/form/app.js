5316923cb5b224b58793121068477c0258c8603b
evilucifero <x@evilucifero.com>
2015-02-16 10:26:20 +0800

    完成表单的模拟提交，已经可以把数据提交到数据库了

--- client/js/form/app.js
+++ client/js/form/app.js
@@ -1,6 +1,8 @@
 function FormCtrl ($scope,$resource,$location,$window) {
   var formUrlSearchObj = $location.search();
   var Form = $resource('/api/forms/:id');
+  var FormResult = $resource('/api/FormResults/');
+  $scope.answer = [];
   $scope.cnFormat = "yyyy'年'MM'月'dd'日 'HH'时'mm'分'";
   Form.get({
       "id":formUrlSearchObj.id
@@ -18,18 +20,27 @@ function FormCtrl ($scope,$resource,$location,$window) {
   $scope.questionShow = function (questionType,showType) {
     return questionType === showType;
   };
-
-/*
-  var FormResult = $resource('/api/FormResults/');
-  $scope.save({
-    },function(res){
-      console.log(res);
-      $scope.form = res;
-    },function (res) {
-      console.log(res);
+  $scope.submit = function () {
+    var resultTmp = [];
+    for (var i=0;i<$scope.answer.length;i++) {
+      resultTmp.push({
+        'questionId': i,
+        'content': $scope.answer[i]
+      });
     }
-  );
-*/
+    FormResult.save({
+        'formId':formUrlSearchObj.id,
+        'weChatUid':'0',
+        'formResultAnswers':resultTmp
+      },
+      function(res){
+        console.log(res);
+      },
+      function (res) {
+        console.log(res);
+      }
+    );
+  };
 }
 var app = angular.module('app', ['ngResource']);
 app.controller('FormCtrl',['$scope','$resource','$location','$window',FormCtrl]);
