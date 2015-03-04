93f7773f309fe7ba12613fe71ed5b1ac8287ec8c
evilucifero <x@evilucifero.com>
2015-03-04 12:22:56 +0800

    优化登录页面的逻辑和设计

--- client/js/login/app.js
+++ client/js/login/app.js
@@ -1,20 +1,24 @@
 function LoginCtrl ($scope,$resource,$timeout,$window) {
   var Login = $resource('/api/OrganizationUsers/login');
   $scope.loginBtn = function(){
-    Login.save({
-      "email":$scope.email,
-      "password":$scope.password
-    },function(res){
-      console.log(res);
-      $window.localStorage.setItem('accessToken',res.id);
-      $window.localStorage.setItem('userId',res.userId);
-      $window.localStorage.setItem('loginTime',res.created);
-      alert('模拟登录成功!');
-      $timeout(function(){$window.location='/admin/index.html'},5000);
-    },function (res) {
-      console.log(res);
-      alert('模拟登录失败!');
-    });
+    Login.save(
+      {
+        'email':$scope.email,
+        'password':$scope.password
+      },
+      function(res){
+        console.log(res);
+        $window.localStorage.setItem('accessToken',res.id);
+        $window.localStorage.setItem('userId',res.userId);
+        $window.localStorage.setItem('loginTime',res.created);
+        alert('模拟登录成功，将在1秒钟后跳转!');
+        $timeout(function(){$window.location='/admin/index.html'},1000);
+      },
+      function (res) {
+        console.log(res);
+        alert('模拟登录失败!');
+      }
+    );
   }
 }
 var app = angular.module('app', ['ngResource']);
--- client/js/login/wcapp.js
+++ client/js/login/wcapp.js
@@ -1,20 +1,24 @@
 function LoginCtrl ($scope,$resource,$timeout,$window) {
   var Login = $resource('/api/WeChatUsers/login');
   $scope.loginBtn = function(){
-    Login.save({
-      "email":$scope.email,
-      "password":$scope.password
-    },function(res){
-      console.log(res);
-      $window.localStorage.setItem('weChatAccessToken',res.id);
-      $window.localStorage.setItem('weChatUid',res.userId);
-      $window.localStorage.setItem('weChatLoginTime',res.created);
-      alert('模拟微信登录成功!');
-      $timeout(function(){$window.location='/list-tmp.html'},5000);
-    },function (res) {
-      console.log(res);
-      alert('模拟模拟登录失败!');
-    });
+    Login.save(
+      {
+        'email':$scope.email,
+        'password':$scope.password
+      },
+      function(res){
+        console.log(res);
+        $window.localStorage.setItem('weChatAccessToken',res.id);
+        $window.localStorage.setItem('weChatUid',res.userId);
+        $window.localStorage.setItem('weChatLoginTime',res.created);
+        alert('模拟微信登录成功，将在1秒钟后跳转!');
+        $timeout(function(){$window.location='/list-tmp.html'},1000);
+      },
+      function (res) {
+        console.log(res);
+        alert('模拟模拟登录失败!');
+      }
+    );
   }
 }
 var app = angular.module('app', ['ngResource']);
