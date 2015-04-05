0377b837df97c9ff8cf98b8dbd3ff70f2fc92a1e
liuJD <erchuochuo@163.com>
2015-04-05 20:47:16 +0800

    no message

--- server/server.js
+++ server/server.js
@@ -11,25 +11,6 @@ var app = module.exports = loopback();
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname);
 
-///图片上传,记得用/common/mosules/ueditor.js替换/node_modules/ueditor/index.js
-///所有图片上传都用这个,img_url 开始结尾不加"/",ueditor修改ueditor.config.js中serverUrl: "/ue/uploads"
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
-
-app.use("/ue/uploads", ueditor( __dirname, function(req, res, next) {
-  if (req.query.action === 'uploadimage') {
-        var foo = req.ueditor;
-        var date = new Date();
-        var imgname = req.ueditor.filename;
-        var img_url = 'images/ueditor';
-        res.ue_up(img_url); 
-  }else if(req.query.action === 'config')
-      //ueditor后台配置文件位置
-  	res.send(require('../client/ueditor/config.json'));
-}));
-///图片上传
 app.use(function(req,res,next){
   res.setHeader('X-Powered-By', 'Kyfly');
   next();
