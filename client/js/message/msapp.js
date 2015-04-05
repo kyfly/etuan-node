f44b0d2ce5ed18262a3128bc8f52efc875340b80
evilucifero <x@evilucifero.com>
2015-04-05 19:14:18 +0800

    增加对于标题的controller

--- client/js/message/msapp.js
+++ client/js/message/msapp.js
@@ -1,3 +1,6 @@
+function headCtrl ($scope,$location) {
+  $scope.title = $location.search().t;
+}
 function messageCtrl ($scope,$location) {
   var searchObj = $location.search();
   $scope.icon = searchObj.i;
@@ -18,8 +21,15 @@ function messageCtrl ($scope,$location) {
       $scope.btnText = '退 出';
       $scope.btnType = 'danger'
       $scope.btnClick = function () {
-        alert('exit!');
-        //WeixinJSBridge.call('closeWindow');
+        window.location.href = '/index.html';
+      };
+      break;
+    case 'close':
+      $scope.btnShow = true;
+      $scope.btnText = '关 闭';
+      $scope.btnType = 'danger'
+      $scope.btnClick = function () {   
+        WeixinJSBridge.call('closeWindow');
       };
       break;
     default:
@@ -32,3 +42,4 @@ function messageCtrl ($scope,$location) {
 }
 var app = angular.module('app', []);
 app.controller('messageCtrl',['$scope','$location',messageCtrl]);
+app.controller('headCtrl',['$scope','$location',headCtrl]);
