1cd418754cfc6f9a5fac20b4532a91a822384e99
wrq <348928400@qq.com>
2015-04-08 16:42:57 +0800

    完成模型接口，ＰＤＦ导出字体存在问题

--- common/models/vote-result.js
+++ common/models/vote-result.js
@@ -1,34 +1,69 @@
 module.exports = function(VoteResult) {
 
-  VoteResult.observe('before save', function (ctx, next) {
-    next({"name":"someError", "status":"422", "message":"A error occurred!"})
+  //vote-result检查１．微信绑定　２．最大投票数　３．已经投票
+  VoteResult.observe('before save', function(ctx, next) {
+    var Vote = VoteResult.app.models.Vote;
+    var WeChatUser = VoteResult.app.models.WeChatUser;
+    try {
+      VoteResult.findOne({ where: { voteId: ctx.instance.voteId, weChatUid: ctx.instance.weChatUid }}, function(err, voteResult) {
+        if(voteResult === null) {
+          Vote.findOne({ where: { id: ctx.instance.voteId }}, function(err, vote) {
+            if(ctx.instance.results.length <= vote.maxVote) {
+              if(vote.verifyRule === 'studentId') {
+                WeChatUser.findOne({ where: { id: ctx.instance.weChatUid }}, function(err, weChatUser) {
+                  if(weChatUser.studentId != null) {
+                    next();
+                  }
+                  else {
+                    next({'status': '400', 'content': '需要绑定微信'});
+                  }
+                });
+              }
+              else {
+                next();
+              }
+            }
+            else {
+              next({'status': '400', 'content': '超过最大投票数'});
+            }
+          });
+        }
+        else {
+          next({'status': '400', 'content': '已经投过票了'});
+        }
+      });
+    }
+    catch (e) {
+      next(e);
+    }
   });
 
-	//vote-result保存之后将投票项的票数加一
-	VoteResult.observe('after save', function(ctx, next) {
-		var Vote = VoteResult.app.models.Vote;
+  //vote-result保存之后将投票项的票数加一
+  VoteResult.observe('after save', function(ctx, next) {
+    var Vote = VoteResult.app.models.Vote;
+    try {
+      Vote.findOne({where:{_id:ctx.instance.voteId}},function(err, vote){
+        ctx.instance.results.forEach(function(id){
 
-    Vote.updateAll({_id:ctx.instance.voteId},{'$inc' : {maxVote : 1} }, function(err, instance) {
-      console.log(instance);
-    });
-
-		Vote.findOne({where:{_id:ctx.instance.voteId}},function(err, vote){
-		 	ctx.instance.results.forEach(function(id){
-
-        vote.subitems.findById(id, function(err, instance) {
-          if(err) {
-            next(err);
-          }
-          else {
-            instance.updateAttribute('count', instance.count+1, function(err, instance) {
-              if(err) {
-                next(err);
-              }
-            });
-          }
+          vote.subitems.findById(id, function(err, instance) {
+            if(err) {
+              next(err);
+            }
+            else {
+              instance.updateAttribute('count', instance.count+1, function(err, instance) {
+                if(err) {
+                  next(err);
+                }
+              });
+            }
+          });
         });
-		 	});
-		});
-		next();
-	});
+      });
+    }
+    catch (e) {
+      next(e);
+    }
+    next();
+  });
+
 };
