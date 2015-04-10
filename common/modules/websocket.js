0b3c959a9b08cd6287318b527a44f0cbbd6f6d0d
Zhenfei You <youzhenfei@126.com>
2015-04-10 11:49:10 +0800

    成功抛出错误

--- common/modules/websocket.js
+++ common/modules/websocket.js
@@ -2,18 +2,17 @@ module.exports.start = function (app) {
 
   var io = require('socket.io')(app.server);
 
-  io.use(function(socket, next) {
-    var token = socket.handshake.headers.access_token;
-    if (token != '12345')
-      next(new Error('Authentication error'));
-    else
-      next();
-  });
-
   for (var i = 0; i < 3; i++) {
-    (function() {
+    (function () {
       var order = i;
-      io.of('/seckill/' + i).on('connection', function (socket) {
+      io.of('/seckill/' + i).use(function (socket, next) {
+        var token = socket.handshake.headers.access_token;
+        if (token != '12345') {
+          next(new Error("Authentication failed"));
+        }
+        else
+          next();
+      }).on('connection', function (socket) {
         socket.emit('news', {hello: 'seckill' + order});
         socket.on('my other event', function (data) {
           console.log(data);
