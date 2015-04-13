ecc543c1cb79352d32625b1397986e3a862e680c
Zhenfei You <youzhenfei@126.com>
2015-04-13 16:02:51 +0800

    按照数据库内疯抢的id创建websocket命名空间

--- common/modules/websocket.js
+++ common/modules/websocket.js
@@ -1,24 +1,29 @@
 module.exports.start = function (app) {
 
-  var io = require('socket.io')(app.server);
-
-  for (var i = 0; i < 3; i++) {
-    (function () {
-      var order = i;
-      io.of('/seckill/' + i).use(function (socket, next) {
-        var token = socket.handshake.headers.access_token;
-        if (token != '12345') {
-          next(new Error("Authentication failed"));
-        }
-        else
-          next();
-      }).on('connection', function (socket) {
-        socket.emit('news', {hello: 'seckill' + order});
-        socket.on('my other event', function (data) {
-          console.log(data);
-        });
-      });
-    })()
+  function authentication(socket, next) {
+    var token = socket.handshake.query.access_token;
+    if (token != '12345') {
+      next(new Error("Authentication failed"));
+    }
+    else
+      next();
   }
 
+  var io = require('socket.io')(app.server);
+
+  app.models.Seckill.find({fields: {id: true}}, function (err, data) {
+    for (var i = 0; i < data.length; i++) {
+      (function () {
+        var seckillId = data[i].id;
+        io.of('/seckill/' + seckillId)
+          .use(authentication)
+          .on('connection', function (socket) {
+            socket.emit('news', {hello: seckillId});
+            socket.on('my other event', function (data) {
+              console.log(data);
+            });
+          });
+      })();
+    }
+  });
 };
