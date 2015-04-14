d23f7e0a09a56dbb06bd59f14ef45fb3b4a3853c
Zhenfei You <youzhenfei@126.com>
2015-04-14 23:05:01 +0800

    基本完成疯抢的后端处理，认证和动态添加socket命名空间未完成

--- common/modules/websocket.js
+++ common/modules/websocket.js
@@ -9,32 +9,111 @@ module.exports.start = function (app) {
       next();
   }
 
-  var io = require('socket.io')(app.server);
+  function verifyIdMask(verifyId) {
+    return verifyId.slice(0, 2) + '***' + verifyId.slice(5, verifyId.length);
+  }
 
-  app.models.Seckill.find({fields: {id: true}}, function (err, data) {
+  var io = require('socket.io')(app.server);
+  var Seckill = app.models.Seckill;
+  var SeckillResult = app.models.SeckillResult;
+
+
+  Seckill.find({fields: {id: true, seckillArrangements: true}}, function (err, data) {
     for (var i = 0; i < data.length; i++) {
       (function () {
         var seckillId = data[i].id;
-        io.of('/seckill/' + seckillId)
-          .use(authentication)
-          .on('connection', function (socket) {
-            app.models.Seckill.findOne({where: {id: seckillId}}, function (err, info) {
-              app.models.SeckillResult.find({
-                where: {seckillId: seckillId},
-                fields: {arrangementId: true, verifyId: true}
-              }, function (err, result) {
-                if (result) {
-                  var verifyId = '';
-                  for (var i = 0; i < result.length; i++) {
-                    verifyId = result[i].verifyId;
-                    result[i].$verifyId = verifyId.slice(0, 2) + '***' + verifyId.slice(5, verifyId.length);
-                  }
-                }
-                info.serverTime = new Date().getTime();
-                socket.emit('initSeckill', info, result);
+        var seckillArrangments = data[i].seckillArrangements;
+        var currentArrangementId = 0;
+        var seckillRemain = 0;
+        SeckillResult.count({and: [{seckillId: seckillId}, {isGet: true}]}, function (err, resultCount) {
+          //找出当前在第几场抢疯抢，以及本场余量（如果下一场已经开始，本场还有余量，继续本场）
+          var tempCount = 0;
+          seckillArrangments.forEach(function (arrangement, index) {
+            tempCount += arrangement.total;
+            if (tempCount > resultCount) {
+              currentArrangementId = index;
+              seckillRemain = tempCount - resultCount;
+              return;
+            }
+          });
+
+          io.of('/seckill/' + seckillId)
+            .use(authentication)
+            .on('connection', function (socket) {
+              Seckill.findOne({where: {id: seckillId}}, function (err, info) {
+                SeckillResult.find({
+                  where: {and: [{seckillId: seckillId}, {isGet: true}]},
+                  fields: {arrangementId: true, verifyId: true}
+                }, function (err, result) {
+                  for (var i = 0; i < result.length; i++)
+                    result[i].verifyId = verifyIdMask(result[i].verifyId)
+                  info.current = currentArrangementId;
+                  info.remain = seckillRemain;
+                  info.serverTime = new Date().getTime();
+                  socket.emit('initSeckill', info, result);
+                });
+              });
+
+              socket.on('addKiller', function (verifyId) {
+                SeckillResult.findOne({
+                    where: {and: [{verifyId: verifyId}, {seckillId: seckillId}, {isGet: true}]},
+                    fields: {verifyId: true}
+                  },
+                  function (err, result) {
+                    if (result)
+                      //error: 已经抢过票了
+                      socket.emit('killFail', 'already gotten');
+                    else
+                      Seckill.findOne({
+                        where: {id: seckillId},
+                        fields: {seckillArrangements: true}
+                      }, function (err, info) {
+                        if (new Date().getTime() <=
+                          new Date(seckillArrangments[currentArrangementId].startTime).getTime())
+                          //error: 还没有开始
+                          socket.emit('killFail', 'not started');
+                        else if (seckillRemain <= 0) {
+                          //error: 没有余票了
+                          socket.emit('killFail', 'no enough');
+                          if (currentArrangementId < seckillArrangments.length) {
+                            currentArrangementId++;
+                            seckillRemain = seckillArrangments[currentArrangementId].total;
+                          }
+                          //没有抢到票的也写入数据库，作为记录和分析
+                          SeckillResult.upsert({
+                            seckillId: seckillId,
+                            arrangementId: currentArrangementId,
+                            verifyId: verifyId,
+                            ip: socket.handshake.address,
+                            isGet: false  //没有抢到
+                          }, function (err) {
+                          });
+                        }
+                        else {
+                          seckillRemain--;
+                          SeckillResult.upsert({
+                            seckillId: seckillId,
+                            arrangementId: currentArrangementId,
+                            verifyId: verifyId,
+                            ip: socket.handshake.address,
+                            isGet: true  //抢到了
+                          }, function (err) {
+                            if (err) {
+                              seckillRemain++;
+                              //error: 写入数据库出错
+                              socket.emit('killFail', 'database error');
+                            }
+                            else {
+                              socket.emit('killSuccess');
+                              io.of('/seckill/' + seckillId).emit('addResult', verifyIdMask(verifyId))
+                            }
+                          });
+                        }
+                      });
+                  });
               });
             });
-          });
+        });
       })();
     }
   });
