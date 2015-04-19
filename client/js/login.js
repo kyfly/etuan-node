75bd4483c96608c19b95219ffb7f81a75f591329
evilucifero <x@evilucifero.com>
2015-04-19 18:47:30 +0800

    修复秒和毫秒的转换错误

--- client/js/login.js
+++ client/js/login.js
@@ -2,7 +2,7 @@ function loginCheck (t) {
   var tmpInfo = JSON.parse(window.localStorage.getItem(t));
   if (!tmpInfo || !tmpInfo.accessToken || !tmpInfo.userId
     || !tmpInfo.loginTime || !tmpInfo.ttl
-    || (new Date() - new Date(tmpInfo.loginTime) > tmpInfo.ttl)
+    || (new Date() - new Date(tmpInfo.loginTime) > tmpInfo.ttl * 1000)
   ) {
     window.localStorage.removeItem(t);
     switch (t) {
