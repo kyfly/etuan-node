e3dc962052a2b19e320ad7d404c55d2d5e20cc59
likegun <likegun94@gmail.com>
2015-04-26 10:30:52 +0800

    结果写入前将结果中IP重写

--- common/models/seckill.js
+++ common/models/seckill.js
@@ -8,5 +8,18 @@ module.exports = function(Seckill) {
       ctx.data.updatedAt = new Date();
     }
     next();
-  });	
+  });
+
+  //创建抢票结果之前写入ip
+  Seckill.beforeRemote('prototype.__create__results', function(ctx, instance, next) {
+    ctx.req.body.ip = getClientIp(ctx.req);
+    next();
+  });
+
+  function getClientIp(req) {
+      return req.headers['x-forwarded-for'] ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      req.connection.socket.remoteAddress;
+  };    
 };
--- common/models/we-chat-user.js
+++ common/models/we-chat-user.js
@@ -161,7 +161,26 @@ module.exports = function(WeChatUser) {
       ctx.data.updatedAt = new Date();
     }
     next();
-  });	  
+  });
+
+  //创建表单结果之前写入ip
+  WeChatUser.beforeRemote('prototype.__create__formResults', function(ctx, instance, next) {
+  	ctx.req.body.ip = getClientIp(ctx.req);
+  	next();
+  });
+
+  //创建投票结果之前写入ip
+  WeChatUser.beforeRemote('prototype.__create__formResults', function(ctx, instance, next) {
+  	ctx.req.body.ip = getClientIp(ctx.req);
+  	next();
+  });  
+
+  function getClientIp(req) {
+      return req.headers['x-forwarded-for'] ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      req.connection.socket.remoteAddress;
+  };  
 
 };
 // {
