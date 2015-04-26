a63fa703d90f9d0a20b48885b36ed881637fe403
likegun <likegun94@gmail.com>
2015-04-26 14:56:49 +0800

    base-item中写入更新时间,移除继承的模块的更新时间hook

--- common/models/base-item.js
+++ common/models/base-item.js
@@ -1,3 +1,11 @@
 module.exports = function(BaseItem) {
-
+	BaseItem.observe('before save', function(ctx, next) {
+    if(ctx.instance) {
+      ctx.instance.updatedAt = new Date();
+    }
+    else {
+      ctx.data.updatedAt = new Date();
+    }
+    next();
+	});
 };
