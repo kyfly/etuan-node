0d4d5a315eac43fc32ffefafb280faff54f4833c
LiC <1078054388@qq.com>
2015-05-05 16:20:42 +0800

    首页新增活动状态判定。

--- client/js/index.js
+++ client/js/index.js
@@ -1,21 +1,57 @@
+
 var app = angular.module('app', ['ui.bootstrap']);
+var nowTime = new Date().getTime();
 
 app.controller('headCtrl', function ($scope) {
   $scope.isCollapsed = true;
 });
 
 app.controller('contentCtrl', ['$scope', '$http', function ($scope, $http) {
+  $scope.cnFormat = "yyyy-MM-dd HH:mm";
   $http.get('/api/Forms?filter[order]=id%20DESC').success(function (res) {
     $scope.flts = res;
+    for (var i = 0; i < 4; i++) {
+      if (new Date($scope.flts[i].startTime).getTime() > nowTime) {
+        $scope.flts[i].activityStatus = "即将开始";
+        $scope.flts[i].textColor = "warning";
+      } else if (new Date($scope.flts[i].stopTime).getTime() < nowTime) {
+        $scope.flts[i].activityStatus = "已经结束";
+        $scope.flts[i].textColor = "danger";
+      } else {
+        $scope.flts[i].activityStatus = "正在进行";
+        $scope.flts[i].textColor = "success";
+      }
+    }
   });
   $http.get('/api/Votes?filter[order]=id%20DESC').success(function (res) {
     $scope.vlts = res;
+    for (var i = 0; i < 4; i++) {
+      if (new Date($scope.vlts[i].startTime).getTime() > nowTime) {
+        $scope.vlts[i].activityStatus = "即将开始";
+        $scope.vlts[i].textColor = "warning";
+      } else if (new Date($scope.vlts[i].stopTime).getTime() < nowTime) {
+        $scope.vlts[i].activityStatus = "已经结束";
+        $scope.vlts[i].textColor = "danger";
+      } else {
+        $scope.vlts[i].activityStatus = "正在进行";
+        $scope.vlts[i].textColor = "success";
+      }
+    }
   });
   $http.get('/api/Seckills?filter[order]=id%20DESC').success(function (res) {
     $scope.skls = res;
+    for (var i = 0; i < 4; i++) {
+      if (new Date($scope.skls[i].seckillArrangements[0].startTime).getTime() > nowTime) {
+        $scope.skls[i].activityStatus = "即将开始";
+        $scope.skls[i].textColor = "warning";
+      } else if ((new Date($scope.skls[i].seckillArrangements[0].startTime).getTime() < nowTime)&&($scope.skls[i].seckillArrangements[$scope.skls[i].seckillArrangements.length-1].total === 0)) {
+        $scope.skls[i].activityStatus = "已经结束";
+        $scope.skls[i].textColor = "danger";
+      } else {
+        $scope.skls[i].activityStatus = "正在进行";
+        $scope.skls[i].textColor = "success";
+      }
+    }
   });
-
-  $scope.cnFormat = "yyyy-MM-dd HH:mm";
-
 
 }]);
