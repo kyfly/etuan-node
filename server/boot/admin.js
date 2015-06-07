ae1e45172b3a82ef3cbc94a96e860b5e60482ac5
liuJD <erchuochuo@163.com>
2015-06-07 18:34:11 +0800

    添加超级管理员角色

---
+++ server/boot/admin.js
@@ -0,0 +1,33 @@
+module.exports = function(app) {
+  var User = app.models.OrganizationUser;
+  var Role = app.models.Role;
+  var RoleMapping = app.models.RoleMapping;
+
+  function createAdmin (userId) {
+    Role.create({
+      name: 'admin'
+    }, function(err, role) {
+      if (err) throw err;
+      role.principals.create({
+        principalType: RoleMapping.USER,
+        principalId: userId
+      }, function(err, principal) {
+        if (err) throw err;
+      });
+    });
+  }
+
+  User.findOne(
+    {where:{email: 'admin@etuan.org'}}
+  , function(err, user) {
+    if (err) throw err;
+    if (!user.id) {
+      User.create({"email":"admin@etuan.org","password":"www.etuan.org"}, function (err, newUser) {
+        createAdmin(newUser.id);
+      });
+    } else {
+      createAdmin(user.id);
+    }
+  });
+};
+//"email":"admin@etuan.org","password":"www.etuan.org"
