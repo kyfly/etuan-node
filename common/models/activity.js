8cd6ae6212bd9ecaf49556d4bd18935a6d456a07
liuJD <erchuochuo@163.com>
2015-07-18 23:45:21 +0800

    新加获取HTML内容接口

--- common/models/activity.js
+++ common/models/activity.js
@@ -5,6 +5,26 @@ module.exports = function(Activity) {
     http: {verb: 'GET', path: '/view/:id'}
   });  
 
+  Activity.remoteMethod('ueContent', {
+    accepts: {arg: 'url', type: 'string'},
+    returns: {arg: 'content', type: 'string'},
+    http: {verb: 'GET', path: '/get-content'}
+  });
+  Activity.ueContent = function (url, cb) {
+    var http = require('http');
+    var req = http.get(url, function(response){
+      var body = [];
+      var headers = response.headers;
+      response.on('data', function (chunk) {
+        body.push(chunk);
+      });
+      response.on('end', function () {
+        body = Buffer.concat(body);
+        cb(null, body.toString());
+      });
+    });
+    req.end();
+  }
   Activity.view = function(id, cb) {
     Activity.update({id: id}, {$inc: {viewCount: 1}}, function(err, result) {
       cb(null);
