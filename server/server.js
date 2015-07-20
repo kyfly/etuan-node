01bc884d3e020619221caaf65d9c359b4a594fbf
likegun <likegun94@gmail.com>
2015-07-20 10:09:29 +0800

    创建活动根据用户自动增加university,school,type字段

--- server/server.js
+++ server/server.js
@@ -2,6 +2,28 @@ var loopback = require('loopback');
 var boot = require('loopback-boot');
 var app = module.exports = loopback();
 
+app.use(loopback.context());
+app.use(loopback.token());
+
+app.use(function setCurrentUser(req, res, next) {
+    if (!req.accessToken) {
+        return next();
+    }
+    app.models.OrganizationUser.findById(req.accessToken.userId, function(err, user) {
+        if (err) {
+          return next(err);
+        }
+        if (!user) {
+          return next(new Error('No user with this access token was found.'));
+        }
+
+        var loopbackContext = loopback.getCurrentContext();
+        if (loopbackContext) {
+              loopbackContext.set('currentUser', user);
+        }
+        next();
+     });
+});
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname);
@@ -27,3 +49,4 @@ app.use(loopback.static('../client'));
 
 var socket = require('../common/modules/websocket.js')(app, server);
 socket.start();
+
