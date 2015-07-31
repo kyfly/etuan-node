6bcb4ce6bc4287dbda78584ec25038f0f9a20886
likegun <likegun94@gmail.com>
2015-07-31 12:20:37 +0800

    投票添加同一学号已经投票验证，拉取后记得cnpm install一下

--- common/models/vote-result.js
+++ common/models/vote-result.js
@@ -1,76 +1,102 @@
 module.exports = function(VoteResult) {
 
-  //vote-result检查１．微信绑定　２．最大投票数　３．已经投票 4.开始时间结束时间
-  VoteResult.observe('before save', function(ctx, next) {
-    var Vote = VoteResult.app.models.Vote;
-    var weChatUser = VoteResult.app.models.WeChatUser;
-    try {
-      VoteResult.findOne({ where: {weChatUid: ctx.instance.weChatUid, voteId: ctx.instance.voteId}}, function(err, voteResult) {
-        if(voteResult === null) {
-          Vote.findOne({ where: { id: ctx.instance.voteId }}, function(err, vote) {
-            if(vote.startTime <= new Date() && vote.stopTime >= new Date()) {
-              if(ctx.instance.results.length <= vote.maxVote) {
-                weChatUser.findOne({where: {id: ctx.instance.weChatUid}}, function(err, weChatUser) {
-                  switch(vote.verifyRule){
-                    case 'studentId':
-                      if(weChatUser.studentId != null) {
-                        next();
-                      }
-                      else {
-                        next({'status': '400', 'message': '需要绑定学号'});
-                      }
-                      break;
-                    default:
-                      next();
-                      break;
-                  }
-                });
-              }
-              else {
-                next({'status': '400', 'message': '超过最大投票数'});
-              }
-            }
-            else {
-              next({'status': '400', 'message': vote.startTime > new Date()?'还未开始': '已经结束'});
-            }
-          });
-        }
-        else {
-          next({'status': '400', 'message': '已经投过票了'});
-        }
-      });
-    }
-    catch (e) {
-      next(e);
-    }
-  });
+    var Q = require('q');
 
-  //vote-result保存之后将投票项的票数加一
-  VoteResult.observe('after save', function(ctx, next) {
-    var Vote = VoteResult.app.models.Vote;
-    try {
-      Vote.findOne({where: {_id: ctx.instance.voteId}},function(err, vote) {
-        ctx.instance.results.forEach(function(id){
-          vote.subitems.findById(id, function(err, instance) {
-            if(err) {
-              next(err);
+    function findOne(model, condition) {
+        return Q.Promise(function(resolve, reject, notify) {
+            model.findOne(condition, function(err, obj) {
+                if(err) {
+                    reject(err);
+                }else {
+                    resolve(obj);
+                }
+            });
+        });
+    }
+
+    //vote-result检查１．微信绑定　２．最大投票数　３．已经投票 4.开始时间结束时间
+    VoteResult.observe('before save', function(ctx, next) {
+        var Vote = VoteResult.app.models.Vote;
+        var WeChatUser = VoteResult.app.models.WeChatUser;
+        findOne(VoteResult, {where: {weChatUid: ctx.instance.weChatUid, voteId: ctx.instance.voteId}})
+        .then(function(voteResult) {
+            if(voteResult === null) {
+                return findOne(Vote, { where: { id: ctx.instance.voteId }});
             }
             else {
-              instance.updateAttribute('count', instance.count+1, function(err, instance) {
-                //这里很神奇的居然会有err但是又能成功更新
-                //if(err) {
-                //  next(err);
-                //}
-              });
+                next({'status': '400', 'message': '已经投过票了'});
             }
-          });
+        })
+        .then(function(vote) {
+            if(vote.startTime <= new Date() && vote.stopTime >= new Date()) {
+                if(ctx.instance.results.length <= vote.maxVote) {
+                    return findOne(WeChatUser, {where: {id: ctx.instance.weChatUid}})
+                                .then(function(weChatUser) {
+                                    return Q.fcall(function() {
+                                        return [vote, weChatUser];
+                                    });
+                                });
+                }
+                else {
+                    next({'status': '400', 'message': '超过最大投票数'});
+                }
+            }
+            else{
+                next({'status': '400', 'message': vote.startTime > new Date()?'还未开始': '已经结束'});
+            }
+        })
+        .then(function(data) {
+            var vote = data[0];
+            var weChatUser = data[1];
+            switch(vote.verifyRule){
+                case 'studentId':
+                    console.log(weChatUser);
+                    if(weChatUser.studentId) {
+                        return findOne(VoteResult, {where: {weChatUid: ctx.instance.weChatUid, studentId: weChatUser.studentId}});
+                    }
+                    else {
+                        next({'status': '400', 'message': '需要绑定学号'});
+                    }
+                    break;
+                default:
+                    next();
+                    break;
+            }
+        })
+        .then(function(voteResult) {
+            if(voteResult) {
+                next({'status': '400', 'message': '已经投过票'});
+            }else {
+                next();
+            }
+        })
+        .fail(function(err) {
+            next(err);
         });
-      });
+    });
+
+//vote-result保存之后将投票项的票数加一
+VoteResult.observe('after save', function(ctx, next) {
+    var Vote = VoteResult.app.models.Vote;
+    try {
+        Vote.findOne({where: {_id: ctx.instance.voteId}},function(err, vote) {
+            ctx.instance.results.forEach(function(id){
+                vote.subitems.findById(id, function(err, instance) {
+                    if(err) {
+                        next(err);
+                    }
+                    else {
+                        instance.updateAttribute('count', instance.count+1, function(err, instance) {
+                        });
+                    }
+                });
+            });
+        });
     }
     catch (e) {
-      next(e);
+        next(e);
     }
     next();
-  });
+});
 
 };
