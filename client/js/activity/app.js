28c5053b0d97da78f8dc7f522b6e83ba435f32cc
LiC <1078054388@qq.com>
2015-07-31 15:20:01 +0800

    更改活动详情显示

--- client/js/activity/app.js
+++ client/js/activity/app.js
@@ -6,13 +6,14 @@ function ActivityCtrl($scope, $resource, $location) {
     "id": activityUrlSearchObj.id
   },
     function (res) {
-      console.log(res);
       $scope.activity = res;
       $scope.title = $scope.activity.title;
       $scope.description = $scope.activity.description;
       $scope.logoUrl = $scope.activity.logoUrl;
       $scope.startTime = new Date($scope.activity.startTime);
       $scope.stopTime = new Date($scope.activity.stopTime);
+      var ueditorContent = $resource('/api/Activities/get-content?url=' + res.contentUrl);
+      document.getElementById("content").innerHTML = ueditorContent;
     },
     function (res) {
     }
