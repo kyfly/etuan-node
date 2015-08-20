74b15ebf99a4cfc2c5d42ca8db6313926787315a
LiC <1078054388@qq.com>
2015-08-20 19:42:45 +0800

    修改疯抢的ngrepeat问题

--- client/js/submit/app.js
+++ client/js/submit/app.js
@@ -14,7 +14,6 @@ function SubmitCtrl($scope, $resource, $timeout, $window) {
           ttl: res.ttl
         };
         $window.localStorage.setItem('d2VjaGF0', JSON.stringify(lsTmp));
-        $window.location = '/admin';
         alert('模拟注册成功，1秒后跳至首页!');
         $timeout(function () {
           $window.location = '/index.html'
